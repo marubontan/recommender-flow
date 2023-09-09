c90d49f185f08b505c330ae321ea62faeea68365
Shuhei Kishi <marubontan@gmail.com>
2023-09-09 16:30:12 -0300

    add simple use case (#1)

--- recommender_flow/domain/model/__init__.py
+++ recommender_flow/domain/model/__init__.py
@@ -1,10 +1,18 @@
 from abc import ABC
 from datetime import datetime
-from uuid import uuid4
-
+from uuid import UUID, uuid4
 
 
 class BaseModel(ABC):
-    def __init__(self):
+    def __init__(self, name):
         self._id = uuid4()
+        self._name = name
         self._created_at = datetime.now()
+
+    @property
+    def id(self) -> UUID:
+        return self._id
+
+    @property
+    def name(self) -> str:
+        return self._name
---
+++ recommender_flow/domain/model/surprise/__init__.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+from surprise import Trainset
+from surprise.prediction_algorithms.predictions import Prediction
+
+from recommender_flow.domain.model import BaseModel
+from recommender_flow.use_case.movie_lens.development.type import TestDataSchema
+
+
+class SurpriseBaseModel(BaseModel, ABC):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+    @abstractmethod
+    def fit(self, data: Trainset):
+        pass
+
+    @abstractmethod
+    def test(self, data: List[TestDataSchema]) -> List[Prediction]:
+        pass
